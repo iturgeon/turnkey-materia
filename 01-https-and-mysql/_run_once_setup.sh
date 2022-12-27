6d3bb97951db025f6693ed6a2c8c9f33d8c2a91d
iturgeon <iturgeon@hey.com>
2022-12-27 13:23:45 +0100

    update run once setup script for https & mysql

--- 01-https-and-mysql/_run_once_setup.sh
+++ 01-https-and-mysql/_run_once_setup.sh
@@ -2,9 +2,11 @@
 openssl req -subj '/CN=localhost' -x509 -newkey rsa:4096 -nodes -keyout ./key.pem -out ./cert.pem -days 365
 
 # create some randomized security keys to store in environment vars
-docker-compose run --rm --no-deps app php -r "echo('AUTH_SIMPLEAUTH_SALT=' . sodium_bin2hex(random_bytes(SODIUM_CRYPTO_STREAM_KEYBYTES)) . \"\n\");" >> .env
 docker-compose run --rm --no-deps app php -r "echo('AUTH_SALT=' . sodium_bin2hex(random_bytes(SODIUM_CRYPTO_STREAM_KEYBYTES)) . \"\n\");" >> .env
 docker-compose run --rm --no-deps app php -r "echo('CIPHER_KEY=' . sodium_bin2hex(random_bytes(SODIUM_CRYPTO_STREAM_KEYBYTES)) . \"\n\");" >> .env
+docker-compose run --rm --no-deps app php -r "echo('CRYPTO_KEY=' . sodium_bin2hex(random_bytes(SODIUM_CRYPTO_STREAM_KEYBYTES)) . \"\n\");" >> .env
+docker-compose run --rm --no-deps app php -r "echo('CRYPTO_IV=' . sodium_bin2hex(random_bytes(SODIUM_CRYPTO_STREAM_KEYBYTES)) . \"\n\");" >> .env
+docker-compose run --rm --no-deps app php -r "echo('CRYPTO_HMAC=' . sodium_bin2hex(random_bytes(SODIUM_CRYPTO_STREAM_KEYBYTES)) . \"\n\");" >> .env
 
 # ask materia to run database migrations and set up initial users
 # this is just running `composer oil-install-quiet` but using a script that will wait for the mysql container to become ready
